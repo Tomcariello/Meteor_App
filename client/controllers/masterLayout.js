ad240bd5bc0874fcc098de36953286a5aafe7016
Tomcariello <tomcariello@yahoo.com>
2016-12-17 12:32:08 -0500

    bugs fixed

--- client/controllers/masterLayout.js
+++ client/controllers/masterLayout.js
@@ -2,6 +2,8 @@ Template.masterLayout.rendered = function () {
   $(".button-collapse").sideNav();
 };
 
-// Template.masterLayout.rendered = function () {
-//   $(".button-collapse").sideNav();
-// };
+Template.masterLayout.events({
+    "click ul#mobile-nav.side-nav li a": function(event){
+      $(".button-collapse").sideNav("hide");
+    }
+});
